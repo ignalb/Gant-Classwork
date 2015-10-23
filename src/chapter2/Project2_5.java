8df5ff4159e1edac3ef80936b2d8e007359b1b7b
Ignacio Pacheco <baxter.ignalb@gmail.com>
2015-10-23 17:46:40 -0400

    Added Chapter 2

---
+++ src/chapter2/Project2_5.java
@@ -0,0 +1,34 @@
+package chapter2;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Ignacio Pacheco
+ *
+ */
+public class Project2_5 {
+	
+	private static Scanner in = new Scanner(System.in);
+	
+	/**
+	 * p = m * v
+	 * @param m Mass
+	 * @param v Velocity
+	 * @return Momentum
+	 */
+	private static double momentum(double m, double v){
+		return m * v;
+	}
+	
+	public static void main (String [] args){
+		double mass, velocity;
+		
+		System.out.print("mass (kg): ");
+		mass = in.nextDouble();
+		System.out.print("velocity (m/s): ");
+		velocity = in.nextDouble();
+		
+		System.out.println("momentum (kg m/s): " + momentum(mass, velocity));
+	}
+}
