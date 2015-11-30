3d9cf38e233ee51674cfed0ca4d307feccc089dc
Ignacio Pacheco <baxter.ignalb@gmail.com>
2015-11-30 01:16:26 -0500

    Edited to fit other changes

--- src/chapter7/Project7_10.java
+++ src/chapter7/Project7_10.java
@@ -5,11 +5,17 @@ import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
+/**
+ * 
+ * @author Ignacio Pacheco
+ *
+ */
 public class Project7_10 {
 
 	public static void main(String[] args){
 		JFrame window = new JFrame("Circle");
-		CirclePanel panel = new CirclePanel(new Circle_Spinning(250, 250, 50, Color.BLUE, 3, 0));
+		CirclePanel panel = new CirclePanel();
+		panel.addCircle(new Circle_Moving(200, 200, 50, Color.BLUE, 5, 0));
 		
 		window.setSize(new Dimension(500, 500));
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
